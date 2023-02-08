7763a1fc48dc47b229782fedc1e3434e70ed579a
neezcha <neezcha@hawaii.edu>
2023-02-07 19:30:54 -1000

    All Recipes page dispaly locally saved recipe

--- src/components/pages/AllRecipiesPage.tsx
+++ src/components/pages/AllRecipiesPage.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import { Recipe } from "../../types/recipe";
 
 
@@ -6,9 +7,36 @@ interface IAllRecipiesPage {
 
 const AllRecipiesPage : React.FC = () => {
 
+    /*
+    function useRecipeFromLocalStorage (key: string|null, defaultValue=''){
+        const [recipeView, setRecipeView] = React.useState(
+          ()=>{
+            const valueInLocalStorage = window.localStorage.getItem(key) 
+            if(valueInLocalStorage){
+              return JSON.parse(valueInLocalStorage)
+            }return defaultValue
+          } 
+
+        )
+    }*/
+
+    const [recipeView, setRecipeView] = useState<Recipe>(
+        JSON.parse(window.localStorage.getItem('loco moco') ?? '' )
+    );
+
+
     return <div>
         All Recipies!
+
+        <div>
+            <ul> { recipeView.title } </ul>
+            <ul> { recipeView.description } </ul>
+            <ul> { recipeView.steps.map((thisStep)=>{ return <ul> {thisStep.index} . {thisStep.instruction} </ul> }) }</ul>
+
+        </div>
+          
+
      </div>; 
 }
 
-export default AllRecipiesPage; 
+export default AllRecipiesPage;
