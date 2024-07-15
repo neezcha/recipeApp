0abf0de7490e8cfdea067be17fe4ef590e3ab623
neezcha <neezcha@hawaii.edu>
2024-07-14 14:54:58 -1000

    remove cms design systems and repalced with radix

--- src/components/pages/AddRecipePage.tsx
+++ src/components/pages/AddRecipePage.tsx
@@ -7,7 +7,7 @@ import RenderIngredient from "../Iterators/UpdateIngredientObj";
 import { json } from "stream/consumers";
 import RenderStep from "../Iterators/UpdateStepsObj";
 import isEqual from "lodash/isEqual";
-import { Button, Text, TextField, TextArea, Select, Strong } from '@radix-ui/themes'
+import { Card, Button, Box, Text, TextField, TextArea, Heading, Flex, Grid, Select, Separator, Strong } from '@radix-ui/themes'
 
 
 interface IAddRecipePage {
@@ -57,82 +57,97 @@ const AddRecipePage : React.FC = () => {
     //current largest index 
     const stepIndexMax = stepsList.length
 
-    return <div>
-        <h2> Add Recipe! </h2>
+    return <>
+        <Heading as="h2"> Add Recipe! </Heading>
 
-        <form> 
-            <ul>
-            <Text as="label"><Strong>RecipeTitle:</Strong></Text>
-            <TextField.Root
-                    name="newRecipeTitleInputFeild"
-                    onChange={(e)=>setNewRecipe({ ...newRecipe, title:e.currentTarget.value})} 
-                    value={newRecipe.title}
-                    placeholder={"Enter Recipe Title"}
-                    />
-            </ul>
-
-            <ul>
+        <Grid p="5"  gap="3"> 
+            <Box>
+                <Text as="label"><Strong>RecipeTitle:</Strong></Text>
+                <TextField.Root
+                        name="newRecipeTitleInputFeild"
+                        onChange={(e)=>setNewRecipe({ ...newRecipe, title:e.currentTarget.value})} 
+                        value={newRecipe.title}
+                        placeholder={"Enter Recipe Title"}
+                        />
+            </Box>
+            <Box>
             <Text as="label"><Strong>Recipe Description:</Strong></Text>
-            <TextArea
-                    name="newRecipeDescInputFeild"
-                    onChange={(e)=>setNewRecipe({ ...newRecipe, description:e.currentTarget.value})} 
-                    value={newRecipe.description}
-                    placeholder={"Enter Recipe Description"}
-                    rows={3}
-                    />
-            </ul>
-            <hr/>
-            <ul className="ds-u-flex-direction--row flex">
-                <h4> Recipe Ingredients </h4>
-                <Text as="label"><Strong>Ingredient Unit:</Strong></Text>
-                <Select.Root 
-                    defaultValue="Unit" 
-                    name="newRecipeIngredientUnitInputFeild" >
-                    <Select.Trigger onChange={(e)=>setnewIngredientUnit({...newIngredientUnit, amount:0, unit:e.currentTarget.value, name:""}) } 
-                    />
-                    <Select.Content>
-                        <Select.Group>
-                        <Select.Label>Fruits</Select.Label>
-                            <Select.Item value="'N/A'">'- Select a Unit of Measurment -</Select.Item>
-                            <Select.Item value="cup">cup</Select.Item>
-                            <Select.Item value="oz" >oz</Select.Item>
-                            <Select.Item value="fl oz">fl oz</Select.Item>
-                            <Select.Item value="tbs" >tbs</Select.Item>
-                            <Select.Item value="tsp" >tsp</Select.Item>
-                        </Select.Group>
-                        <Select.Separator />
-                    </Select.Content>
-                    </Select.Root>
-                <Text as="label"><Strong>Amount of Ingredient:</Strong></Text>
-                <TextField.Root
-                    name="newRecipeIngredientsAmountInputFeild"
-                    autoFocus
-                    size="1"
-                    pattern="[0-9]*"
-                    onChange={(e)=>setNewIngredient({...newIngredient, amount:Number(e.currentTarget.value), unit:"", name:""})} 
-                    value={newIngredient.amount} 
-                    placeholder="Enter Amount of Ingredient"
-                />
-                <Text as="label"><Strong>Ingredient:</Strong></Text>
-                <TextField.Root
-                    name="newRecipeIngredientsNameInputFeild"
-                    onChange={(e)=>setnewIngredientName({...newIngredientName, amount:0, unit:"", name:e.currentTarget.value}) }
-                    value={newIngredientName.name} 
-                    placeholder="Enter Ingredient Name"
-                />
+                <TextArea
+                        name="newRecipeDescInputFeild"
+                        onChange={(e)=>setNewRecipe({ ...newRecipe, description:e.currentTarget.value})} 
+                        value={newRecipe.description}
+                        placeholder={"Enter Recipe Description"}
+                        rows={3}
+                        />
+            </Box>
+            <Separator size="4"/>
+            <Box >
+                <Heading as='h4' size="4"> Recipe Ingredients</Heading>
+            <Box>
+                <Card size="4">
+                    <Grid gap="3" align="center" columns="1">
+                        <Flex gap="3">
+                            <Text as="label"><Strong>Ingredient Unit:</Strong></Text>
+                            <Select.Root 
+                                defaultValue="unit" 
+                                name="newRecipeIngredientUnitInputFeild" >
+                                <Select.Trigger onChange={(e)=>setnewIngredientUnit({...newIngredientUnit, amount:0, unit:e.currentTarget.value, name:""}) } 
+                                />
+                                <Select.Content >
+                                    <Select.Group>
+                                        <Select.Item value="unit" disabled>- Select a Unit of Measurment -</Select.Item>
+                                        <Select.Item value="cup">cup</Select.Item>
+                                        <Select.Item value="oz" >oz</Select.Item>
+                                        <Select.Item value="fl oz">fl oz</Select.Item>
+                                        <Select.Item value="tbs" >tbs</Select.Item>
+                                        <Select.Item value="tsp" >tsp</Select.Item>
+                                    </Select.Group>
+                                    <Select.Separator />
+                                </Select.Content>
+                                </Select.Root>
+                            </Flex>
+                            <Box>    
+                                <Text as="label"><Strong>Amount of Ingredient:</Strong></Text>
+                                <TextField.Root
+                                    name="newRecipeIngredientsAmountInputFeild"
+                                    autoFocus
+                                    size="1"
+                                    pattern="[0-9]*"
+                                    onChange={(e)=>setNewIngredient({...newIngredient, amount:Number(e.currentTarget.value), unit:"", name:""})} 
+                                    value={newIngredient.amount} 
+                                    placeholder="Enter Amount of Ingredient"
+                                />
+                             </Box>
+                             <Box>   
+                                <Text as="label"><Strong>Ingredient:</Strong></Text>
+                                <TextField.Root
+                                    name="newRecipeIngredientsNameInputFeild"
+                                    onChange={(e)=>setnewIngredientName({...newIngredientName, amount:0, unit:"", name:e.currentTarget.value}) }
+                                    value={newIngredientName.name} 
+                                    placeholder="Enter Ingredient Name"
+                                />
+                        </Box>
+                    </Grid>
+                </Card>
+            </Box>
 
+
+                
+                
+                
             <div> 
                 <Button
-                className="ds-u-margin-right--2 ds-u-padding-y--2"
-                onClick={(e)=>setNewRecipe({ ...newRecipe, ingredients:[...newRecipe.ingredients, {amount: Number(newIngredient?.amount), unit:newIngredientUnit.unit, name:newIngredientName?.name}] }) }
+                    className="ds-u-margin-right--2 ds-u-padding-y--2"
+                    onClick={(e)=>setNewRecipe({ ...newRecipe, ingredients:[...newRecipe.ingredients, {amount: Number(newIngredient?.amount), unit:newIngredientUnit.unit, name:newIngredientName?.name}] }) }
                 >➕
                 </Button> 
-                <b> Summary: </b>{newIngredient.amount} {newIngredientUnit.unit}  {newIngredientName.name}
+                <Text> { "Summary: "} {newIngredient.amount} {", "} {newIngredientUnit.unit} {", "} {newIngredientName.name} </Text>
             </div>
-            
-            </ul>
-            <ul>
-            <h3> Ingredients </h3>
+            </Box>
+
+
+            <Box>
+            <Heading as="h3" size="4"> Ingredients </Heading>
             {ingredientList.map((ingredient)=>{
 
                 console.log( JSON.stringify(ingredient))
@@ -142,22 +157,22 @@ const AddRecipePage : React.FC = () => {
                 
                 </div>
             })}
-            </ul>
-            <hr/>
-            <ul>
+            </Box>
+            <Separator size="4"/>
+            <Box>
                 <label className="b"> Steps: </label>
                 {/* <input type={"number"} min="0" max="1" onChange={(e)=>setNewStepIndex({...newStepIndex, index:Number(e.currentTarget.value) })} placeholder="Index"/>  */}
                 {/* <input onChange={(e)=>setNewStepInstruction({...newStepInstruction, instruction:(e.currentTarget.value)} )} placeholder={"Step"} />*/}
                 {/* <button type="button" onClick={(e)=>setNewRecipe({ ...newRecipe, steps:[...newRecipe.steps, {index: Number(newStepIndex?.index), instruction:newStepInstruction.instruction}] }) }> Add Step</button>*/}
-            </ul>
-            <ul>
+            </Box>
+            <Box>
                 {stepsList.map((step)=> {
                     return <div key={step.index+" "+step.instruction}>
                         <RenderStep step={step} recipe={newRecipe} setRecipe={setNewRecipe}/>
                     </div>
                 })}
-            </ul>
-            <ul>
+            </Box>
+            <Box>
             <Text as="label"><Strong>New Step:</Strong></Text>
             <TextField.Root
                         name="newStepInputFeild"
@@ -174,7 +189,7 @@ const AddRecipePage : React.FC = () => {
                     setNewRecipe({ ...newRecipe, steps:[...newRecipe.steps, {index: newStepIndexVar, instruction:newStepInstruction.instruction}] })
                 } }> ➕</Button>
             
-            </ul>
+            </Box>
 
             <Button variant="solid" className="ds-u-margin--1" type={"submit"} onClick={(e)=> {
                 e.preventDefault();
@@ -189,9 +204,9 @@ const AddRecipePage : React.FC = () => {
 
              <Button className="ds-u-margin--1" type="button" onClick={()=> setNewRecipe(blankRecipe) }> Reset Recipe </Button>
             {/*<button type="button" onClick={()=>{<UpdateIngredient ingredients={newRecipe.ingredients} />} }> Show Ingredeints</button>*/}
-        </form>
+        </Grid>
 
-     </div>; 
+     </>; 
 }
 
 export default AddRecipePage; 
