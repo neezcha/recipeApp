92ca6ed0055af40583132006a6dba4bf983af87f
neezcha <neezcha@hawaii.edu>
2025-01-20 10:52:10 -1000

    refactored routing

--- src/components/pages/MainPage.tsx
+++ src/components/pages/MainPage.tsx
@@ -6,36 +6,17 @@ import RecipeItem from "../RecipeItem";
 import NavMenu, { navMenuItemArray } from "../Navigation/Menu";
 import { Box, Flex, Separator } from "@radix-ui/themes";
 import UseDestionation from "../Navigation/UseDest";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useAppState } from "../../app-state";
 
 const MainPage : React.FC = () => {
+    const appState = useAppState();
 
-
-    /************ TO REMOVE: hard coded example recipes */  
-    const milk : Ingredient = {
-        amount: 1, 
-        unit: "cup",
-        name: "milk"
-    }
-    const cereal : Recipe = {
-        title: "Simple Cereal",
-        description:"",
-        ingredients:[
-            milk,
-            {amount:1, unit:"cup", name:"Cereal"} // inline object creation
-        ],
-        steps:[
-            {index:1, instruction:"Gather ingredients."},
-            {index:2, instruction:"Get bowl and a spoon."},
-            {index:3, instruction:"Pour cereal into bowl."},
-            {index:4, instruction:"Pour Milk into bowl."}
-        ] 
-    };
-    const recipes :Recipe[] = [cereal, {...cereal, title: "Complex Cereal"}];
-
-    /*********** END TO DO  *********************************************/
-
+    // const [destination,setDestination] = useState<string>(navMenuItemArray[0].dest);
     const [destination,setDestination] = useState<string>(navMenuItemArray[0].dest);
+    useEffect(()=>{
+        appState.setPageDest(destination);
+    },[destination]);
     
    return <>
         <NavMenu setDestination={setDestination}/>
